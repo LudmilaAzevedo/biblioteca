4c9731333d8c11079efa4d2d47b2a41a66814ca1
LudmilaAzevedo <161759503+LudmilaAzevedo@users.noreply.github.com>
2025-03-21 10:53:49 -0300

    Versionando

---
+++ src/pages/PHome/PHome.tsx
@@ -0,0 +1,15 @@
+import Cabecalho from "../../components/Cabecalho/cabecalho";
+import Rodape from "../../components/Rodape/Rodape";
+import Welcome from "../../components/Welcome/Welcome";
+
+function PHome() {
+    return (
+        <>
+        <Cabecalho/>
+        <Welcome/>
+        <Rodape/>
+        </>
+    );
+}
+
+export default PHome
---
+++ src/pages/PLogin/PLogin.tsx
@@ -0,0 +1,16 @@
+import Cabecalho from "../../components/Cabecalho/cabecalho";
+import LoginForm from "../../components/LoginForm/LoginForm";
+import Rodape from "../../components/Rodape/Rodape";
+
+
+function PHome() {
+    return (
+        <>
+        <Cabecalho/>
+        <LoginForm/>
+        <Rodape/>
+        </>
+    );
+}
+
+export default PHome
